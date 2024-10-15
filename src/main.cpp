7664ab38548b5cb4de51b0ed14ee5dac9bcb8f2a
leonlove <2415630511@qq.com>
2024-10-15 17:00:38 +0800

    定义软件的框架，并创建相应的空白文件

--- src/main.cpp
+++ src/main.cpp
@@ -6,52 +6,6 @@ using namespace std;
 using namespace BBGLOBE;
 
 int main(){
-	cout << "Hello World!" << endl;
-
-	Point2d point2d(1,1);
-	cout << "point.x = " << point2d.x() << " point.y = " << point2d._v[1] << endl;
-
-	Point2d point2dxx(3,4);
-	cout << "point.x = " << point2dxx.x() << " point.y = " << point2dxx._v[1] << endl;
-
-	Point2d point2d3;
-	point2d3 += point2dxx;
-	cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	cout << "point2d3.Length = " << point2d3.Length() << endl;
-
-
-	cout << "point.x = " << point2d3.Normalize().x() << " point.y = " << point2d3.Normalize().y() << endl;
-	
-	point2d3.Set(10, 10);
-	cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-
-	//point2d3 *= 2;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	//point2d3 /= 2;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	//if (point2d < point2dxx)
-	//	cout << " A point.x = " << point2d.x() << " point.y = " << point2d._v[1] << endl;
-	//else
-	//	cout << " B point.x = " << point2dxx.x() << " point.y = " << point2dxx._v[1] << endl;
-
-	//Point2d point2d3 = point2d + point2dxx;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-	//
-	//point2d3 = point2d - point2dxx;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	//point2d3 = point2d3 * 3;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	//point2d3 = point2d3 / 3;
-	//cout << "point.x = " << point2d3.x() << " point.y = " << point2d3._v[1] << endl;
-
-	//Point3d point3d;
-	//cout << "point.x = " << point3d.x() << " point.y = " << point3d._v[1] << endl;
 
 	return 0;
 }
